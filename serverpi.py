8578f1b7f46e551f60b37fb36d593a4f44235ed0
Amayr Babar <amayrbabar@Amayrs-MacBook-Pro.local>
2024-02-01 21:49:16 -0500

    record on time

--- serverpi.py
+++ serverpi.py
@@ -3,13 +3,19 @@
 import os
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import grovepi
+import time
 
 # GrovePi setup
 led = 5  # LED connected to D5
 grovepi.pinMode(led, "OUTPUT")
 
-host_name = '10.0.0.184'  # IP Address of Raspberry Pi
+host_name = '10.2.8.249'  # IP Address of Raspberry Pi
 host_port = 8000
+
+# Initialize variables to track LED state and time
+led_state = False
+start_time = 0
+duration = 0  # Duration for which the LED was on
 
 def getTemperature():
     temp = os.popen("/opt/vc/bin/vcgencmd measure_temp").read()
@@ -28,11 +34,13 @@ class MyServer(BaseHTTPRequestHandler):
         self.end_headers()
 
     def do_GET(self):
-        html = '''
+        global duration
+        html = f'''
            <html>
            <body style="width:960px; margin: 20px auto;">
            <h1>Welcome to my Raspberry Pi</h1>
-           <p>Current GPU temperature is {}</p>
+           <p>Current GPU temperature is {getTemperature()[5:]}</p>
+           <p>LED was on for {duration} seconds.</p>
            <form action="/" method="POST">
                Turn LED :
                <input type="submit" name="submit" value="On">
@@ -41,21 +49,27 @@ class MyServer(BaseHTTPRequestHandler):
            </body>
            </html>
         '''
-        temp = getTemperature()
         self.do_HEAD()
-        self.wfile.write(html.format(temp[5:]).encode("utf-8"))
+        self.wfile.write(html.encode("utf-8"))
 
     def do_POST(self):
+        global led_state, start_time, duration
         content_length = int(self.headers['Content-Length'])  # Gets the size of data
         post_data = self.rfile.read(content_length).decode("utf-8")  # Gets the data itself
         post_data = post_data.split("=")[1]  # Parses the data
 
-        if post_data == 'On':
+        if post_data == 'On' and not led_state:
+            led_state = True
+            start_time = time.time()
             grovepi.digitalWrite(led, 1)  # Turn LED on
-        else:
+        elif post_data == 'Off' and led_state:
+            led_state = False
+            end_time = time.time()
+            duration = round(end_time - start_time, 2)  # Calculate duration in seconds
             grovepi.digitalWrite(led, 0)  # Turn LED off
 
-        print("LED is {}".format(post_data))
+        print(f"LED is {post_data}")
+        print(f"LED was on for {duration} seconds.") if duration else print("")
         self._redirect('/')  # Redirect back to the root url
 
 # Main
